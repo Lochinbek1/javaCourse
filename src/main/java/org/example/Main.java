10df51984ce3d449ba5f4301c8c3e2822a5737c4
Qodirov Lochinbek <lochinbek433@gmail.com>
2022-07-15 20:37:17 +0500

    This is another test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi there, My name is Qodirov Lochinbek. This is test.");
+        System.out.println("This is another test");
     }
 }
